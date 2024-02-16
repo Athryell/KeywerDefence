0c51251ec5510d17292908878deca0f4aebd0e24
Athryell <93521016+Athryell@users.noreply.github.com>
2024-02-16 17:37:26 +0100

    Update key.gd

--- Scenes/key.gd
+++ Scenes/key.gd
@@ -1,33 +1,37 @@
 extends Node2D
 class_name KeyScene
 
-const SPRITE_BASE_PATH_ACTIVE := "res://Sprites/Spritesheets Letters/"
+#@onready var key_sprite = $Key_sprite
+
 const SPRITE_BASE_PATH_NOT_ACTIVE := "res://Sprites/Spritesheets Letters Disabled/"
 const SPRITE_FORMAT_PATH := ".png"
 
+var key_input_bind: String
+var is_active: bool
+var weapon
 
-var key_bind: String
-var override_key: String
-var weapon: Resource
+func set_button_bind(input_key):
+	key_input_bind = input_key
+	$Key_sprite.texture = load(SPRITE_BASE_PATH_NOT_ACTIVE + input_key + SPRITE_FORMAT_PATH)
+	$Key_sprite.set_hframes(3)
 
-@onready var sprite := $Weapon/Sprite2D
-
-func _ready():
-	set_key(key_bind)
-
-
-func set_key(letter):
-	override_key = letter
-	set_sprite(override_key)
-	set_weapon(override_key)
 
 func set_weapon(letter):
-	$Weapon.weapon_resource = WeaponDatabase.get_weapon(letter)
+	var new_weapon = load("res://Scenes/Weapons/Weapon_" + letter + ".tscn")
+	if not new_weapon:
+		return
+	weapon = new_weapon.instantiate()
+	$Key_sprite.texture = weapon.get_texture()
 	
+	add_child(weapon)
+
+
+func remove_weapon():
+	weapon = null
 
 
 func _unhandled_input(event):
-	if event is InputEventKey and event.as_text_keycode() == key_bind.to_upper():
+	if event is InputEventKey and event.as_text_keycode() == key_input_bind.to_upper():
 		if event.pressed and not event.echo:
 			handle_key_press()
 		elif event.is_released():
@@ -35,22 +39,10 @@ func _unhandled_input(event):
 
 
 func handle_key_press():
-	sprite.frame = 1
-	$Weapon.shoot()
+	$Key_sprite.frame = 1
+	if weapon:
+		weapon.shoot()
 
 
 func handle_key_released():
-	sprite.frame = 0
-
-var is_active
-
-func set_sprite(key):
-	var base_path
-	if is_active:
-		base_path = SPRITE_BASE_PATH_ACTIVE
-	else:
-		base_path = SPRITE_BASE_PATH_NOT_ACTIVE
-		
-	var sprite_path = base_path + key + SPRITE_FORMAT_PATH
-	sprite.texture = load(sprite_path)
-
+	$Key_sprite.frame = 0
