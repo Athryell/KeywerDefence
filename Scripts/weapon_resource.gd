5dd9d3d216f569e06fa2dd480102cd15f37a4a08
Athryell <93521016+Athryell@users.noreply.github.com>
2024-02-16 17:38:46 +0100

    Update weapon_resource.gd

--- Scripts/weapon_resource.gd
+++ Scripts/weapon_resource.gd
@@ -12,12 +12,13 @@ extends Resource
 @export var damage: int
 @export var speed: float
 @export var recharge_time: float
+@export var area_range: float = 0
 
-var is_active: bool
+var is_weapon_active: bool
 
 var _weaponLetter: String
 var _canShoot: bool
 
 func shoot():
-	if is_active:
+	if is_weapon_active:
 		print("'_shootBullet' no implemented for", weaponName)
