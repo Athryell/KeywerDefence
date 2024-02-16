a11dc20bd919e9e64b1c65b3a5eb0a3362335504
Athryell <93521016+Athryell@users.noreply.github.com>
2024-02-16 17:38:13 +0100

    Update Keyboard.gd

--- Scenes/Keyboard.gd
+++ Scenes/Keyboard.gd
@@ -18,15 +18,15 @@ func init_keyboard():
 	for row in range(keyboard_layout.size()):
 		for col in range(keyboard_layout[row].length()):
 			var letter = keyboard_layout[row][col]
-			var new_key = key_scene.instantiate()
+			var new_key = key_scene.instantiate() as KeyScene
 			new_key.name = "Key_" + letter
-			new_key.key_bind = letter
+			new_key.set_button_bind(letter)
 			new_key.position.x = col * KEY_DIMENSIONS + row * 18
 			new_key.position.y = row * KEY_DIMENSIONS
 
 			if letter in GameManager.get_discovered_letters():
-				new_key.is_active = true
-				new_key.get_node("Weapon").is_active = true
+				new_key.set_weapon(letter)
+			
 			add_child(new_key)
 
 
