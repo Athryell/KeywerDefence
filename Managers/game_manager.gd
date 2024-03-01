bdddcf4989d5f67056c888f2e01566bf664f31b5
Athryell <93521016+Athryell@users.noreply.github.com>
2024-03-01 16:21:37 +0100

    Added debug input 1-2-3-4 for GameStates

--- Managers/game_manager.gd
+++ Managers/game_manager.gd
@@ -1,9 +1,9 @@
 extends Node
 
 var starting_letters = "I"
-var _letters_discovered = ["L", "T", "F", "X", "E"] #"V", "U", "C", "S", "W"
+var _letters_discovered = ["L", "T", "F", "X", "E", "V"] #"U", "C", "S", "W"
 
-var is_merger_open := false
+@onready var game_state = StateMachine
 
 func _ready():
 	add_discovered_letter(starting_letters)
@@ -15,13 +15,17 @@ func add_discovered_letter(letter):
 	_letters_discovered.append(letter)
 
 
-func _input(event):
-	if event.is_action_pressed("open_merger"):
+func _input(event): 
+	if event.is_action_pressed("debug_gameState1"): #TODO: debug
 		if event.pressed and not event.echo:
-			is_merger_open = not is_merger_open
+			game_state.change_state(game_state.GameState.DEFENDING)
+	if event.is_action_pressed("debug_gameState2"):  #TODO: debug
+		if event.pressed and not event.echo:
+			game_state.change_state(game_state.GameState.MERGING_LETTERS)
+	if event.is_action_pressed("debug_gameState3"):  #TODO: debug
+		if event.pressed and not event.echo:
+			game_state.change_state(game_state.GameState.PLACING_KEYS)
+	if event.is_action_pressed("debug_gameState4"):  #TODO: debug
+		if event.pressed and not event.echo:
+			game_state.change_state(game_state.GameState.MAIN_MENU)
 		
-		if is_merger_open:
-			get_tree().change_scene_to_file("res://Scenes/UI/LetterMergeUI.tscn")
-		else:
-			get_tree().change_scene_to_file("res://Scenes/main.tscn")
-
