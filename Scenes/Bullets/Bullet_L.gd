5d527c60c55131199bef71d60e8a6c7911bd4397
Athryell <93521016+Athryell@users.noreply.github.com>
2024-03-01 16:24:30 +0100

    Update Bullet_L.gd

--- Scenes/Bullets/Bullet_L.gd
+++ Scenes/Bullets/Bullet_L.gd
@@ -1,9 +1,17 @@
 extends BaseBullet
-
+	
+	
+func _ready():
+	super()
+	
+	var rand_rot = randf_range(-10, 10)
+	rotation += deg_to_rad(rand_rot)
+	scale *= randf_range(0.5, 2)
+	
 func _physics_process(delta):
 	super(delta)
 	
-	var direction = Vector2.RIGHT.rotated(rotation)
+	var direction = Vector2.RIGHT.rotated(rotation).normalized()
 	position += direction * SPEED * delta
 
 
